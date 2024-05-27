55abc6faa6bd3b944d0ca0f3d7cb707398673eeb
Gabriel Ferrer <gefv1891@gmail.com>
2024-05-26 23:42:58 -0400

    primer commit

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { StyleSheet, Text, View } from 'react-native'
+import { withAuthenticator } from '@aws-amplify/ui-react-native'
+import { Amplify, Auth } from 'aws-amplify'
+import awsConfig from './src/aws-exports'
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+Amplify.configure(awsConfig)
+
+const App = () => {
+	return (
+		<View style={styles.container}>
+			<Text style={{ fontSize: 32, fontWeight: 'bold' }}>Home screen</Text>
+			<StatusBar style='auto' />
+		</View>
+	)
 }
 
+export default withAuthenticator(App)
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+})
---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "onlychatgabrielbcfb5e902d": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
