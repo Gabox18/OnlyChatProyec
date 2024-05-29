92b1e5499e0ab0ce654cf925a32c55fcd64656e4
Gabriel Ferrer <gefv1891@gmail.com>
2024-05-28 23:29:52 -0400

    iniciando el use context

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import { withAuthenticator } from '@aws-amplify/ui-react-native'
+//import { withAuthenticator } from '@aws-amplify/ui-react-native'
 import { Amplify, Auth } from 'aws-amplify'
 import awsConfig from './src/aws-exports'
+import SignIn from './src/components/Signin'
 
 Amplify.configure(awsConfig)
 
 const App = () => {
 	return (
 		<View style={styles.container}>
-			<Text style={{ fontSize: 32, fontWeight: 'bold' }}>Home screen</Text>
-			<StatusBar style='auto' />
+			<SignIn />
 		</View>
 	)
 }
 
-export default withAuthenticator(App)
+export default App
 
 const styles = StyleSheet.create({
 	container: {
