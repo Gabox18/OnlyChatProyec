0b13826d8512c9dab91f4f829a85b594af565c80
Gabriel Ferrer <gefv1891@gmail.com>
2024-06-12 21:16:41 -0400

    sin aplicar modo oscuro

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
-//import { withAuthenticator } from '@aws-amplify/ui-react-native'
-import { Amplify, Auth } from 'aws-amplify'
+import * as React from 'react'
 import awsConfig from './src/aws-exports'
-import SignIn from './src/components/Signin'
+import { withAuthenticator } from '@aws-amplify/ui-react-native'
+import { Amplify, Auth, Hub } from 'aws-amplify'
+import awsconfig from './src/aws-exports'
+import Root from './src/navigation/Root'
+import Splash from './src/screens/Splash'
+import AuthScreen from './src/screens/Auth'
 
-Amplify.configure(awsConfig)
 
-const App = () => {
-	return (
-		<View style={styles.container}>
-			<SignIn />
-		</View>
-	)
+Amplify.configure(awsconfig)
+
+export default function App() {
+	const [user, setUser] = React.useState(null)
+	const [isLoading, setIsLoading] = React.useState(true)
+
+	const listener = data => {
+		switch (data.payload.event) {
+			case 'signIn':
+				const { attributes } = data.payload.data
+				console.log('hello world', attributes)
+				setUser(attributes)
+				console.log('user signed in')
+				break
+			case 'signOut':
+				setUser(null)
+				console.log('user signed out')
+				break
+			default:
+				break
+		}
+	}
+
+	//Hub.listen('auth', listener)
+
+	if (isLoading) return <Splash setUser={setUser} setIsLoading={setIsLoading} />
+	return user ? <Root user={user} /> : <AuthScreen />
 }
-
-export default App
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-})
